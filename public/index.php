3c1fc7815c961aa4a8680c67fe7179097b6665a6
Anand Pilania <pilaniaanand@gmail.com>
2017-10-03 01:02:36 +0530

    vendor dir updated at artisan, public/index.php & socialite, cashier and passport added as composer dependency

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../vendor-5.5/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
